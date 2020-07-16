2ac07d6603491aefee243df68e8764eb35c33fa3
hollandaise1 <xinzhuli.cmu@gmail.com>
2020-07-15 19:42:02 -0700

    Update coursera_python_basics_week3.py

--- coursera_python_basics_week3.py
+++ coursera_python_basics_week3.py
@@ -52,17 +52,17 @@ Note 1: be sure to not double-count words that contain both an a and an e.
 HINT 1: Use the in operator.
 HINT 2: You can either use or or elif.
 sentence = "python is a high level general purpose programming language that can be applied to many different classes of problems."
-
+sentence_split = sentence.split(' ')
 num_a_or_e = 0
 
-for item in sentence:
+for item in sentence_split:
     if "a" in item:
         num_a_or_e += 1
     elif "e" in item:
         num_a_or_e += 1
 print(num_a_or_e)
 
-# this code is incomplete, it prints 22 while the answer is 14
+# answer is: 14
 
 # problem:
 Write code that will count the number of vowels in the sentence s and assign the result to the variable num_vowels. For this problem, vowels are only a, e, i, o, and u. Hint: use the in operator with vowels.
